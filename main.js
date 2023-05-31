081c27bef586f3e4634b9e49e9f4b6b754a3e9ad
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-05-31 13:07:01 -0600

    add about page mobile

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+// ----------BURGER MENU----------------- //
+const navbar = document.querySelector('#nav-bar');
+const burger = document.querySelector('#hamb');
+const menuLinks = document.querySelectorAll('.nav_links a');
+
+let isMenuVisible = false;
+
+function toggleMenu() {
+  if (!isMenuVisible) {
+    navbar.classList.remove('navbar');
+    navbar.classList.add('nav-bar-active');
+    burger.classList.remove('hamb');
+    burger.classList.add('nav-bar_menu-active');
+
+    // Update menu state
+    isMenuVisible = true;
+  } else {
+    // eslint-disable-next-line no-use-before-define
+    closeMenu();
+
+    // Update menu state
+    isMenuVisible = false;
+  }
+}
+
+function closeMenu() {
+  navbar.classList.remove('nav-bar-active');
+  navbar.classList.add('navbar');
+  burger.classList.remove('nav-bar_menu-active');
+  burger.classList.add('nav-bar_menu');
+
+  // Update menu state
+  isMenuVisible = false;
+}
+
+burger.addEventListener('click', toggleMenu);
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    if (isMenuVisible) {
+      closeMenu();
+    }
+  });
+});
+
+// ----------LINEUP SHOW MORE BUTTON----------------- //
 const seeMoreButton = document.querySelector('.see-more');
 const lineupSectionCards = document.querySelectorAll('.lineup_section_card');
 
